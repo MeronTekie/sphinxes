c071c42353ba521f2e66e9a100fab902560b82d6
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-02 13:56:59 -0700

    Modify the Program file
    co-authored-by: Evgeniya Meshuris <em@theemm.com>
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Sphinx
+{
+    static void Main()
+    {
+        Console.WriteLine("What can bring back the dead; make you cry, make you laugh, make you young; is born in an instant, yet lasts a lifetime");
+        string answer = Console.ReadLine();
+        answer.ToUpper();
+
+        if(answer =="MEMORY")
+        {
+            Console.WriteLine(" Never resting, never still; moving silently from hill to hill; it does not walk, run or trot; all is cool where it is not");
+            string answer1 = Console.ReadLine();
+            answer1.ToUpper();
+
+            if (answer1 == "SUN") 
+            {
+                Console.WriteLine("What can bring back the dead; make you cry, make you laugh, make you young; is born in an instant, yet lasts a lifetime");
+                string answer2 = Console.ReadLine();
+                answer2.ToUpper();
+
+            }
+            else
+            {
+                Console.WriteLine("You answered one hopefully you will do beter next time");
+            }
+        }
+        else
+        {
+            Console.WriteLine("You answered none hopefully you will do beter next time");
+        }
+    }
+}
+
+
